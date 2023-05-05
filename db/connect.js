af2b90b4d8c78ab20b4480d2176b8720a4cdee1c
hardeymolar <abdulmalikgbolahan95@gmail.com>
2023-05-05 13:27:10 +0100

    new task manager project

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+const connectDB=(url)=>{
+    mongoose
+    .connect(url,{
+        useCreateIndex:true,
+        useFindAndModify:false,
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+}
+
+module.exports = connectDB
